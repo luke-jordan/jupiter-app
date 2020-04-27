810c326c00856603e357fde43726621be13e88b0
luke-grassroot <luke.jordan@grassroot.org.za>
2020-04-27 17:29:33 +0200

    Initiated basic store structure, core backend calls

---
+++ src/modules/friend/friend.actions.js
@@ -0,0 +1,20 @@
+export const namespace = 'FRIENDS';
+
+export const UPDATE_FRIEND_ALERT_PENDING = `${namespace}/UPDATE_FRIEND_ALERT_PENDING`;
+export const UPDATE_FRIEND_LIST = `${namespace}/UPDATE_FRIEND_LIST`;
+export const UPDATE_FRIEND_REQUEST_LIST = `${namespace}/UPDATE_FRIEND_REQUEST_LIST`;
+
+export const updateFriendAlertPending = isPending => ({
+  type: UPDATE_FRIEND_ALERT_PENDING,
+  isPending,
+});
+
+export const updateFriendList = friendList => ({
+  type: UPDATE_FRIEND_LIST,
+  friendList,
+});
+
+export const updateFriendReqList = friendRequestList => ({
+  type: UPDATE_FRIEND_REQUEST_LIST,
+  friendRequestList,
+});
---
+++ src/modules/friend/friend.service.js
@@ -0,0 +1,65 @@
+import { Endpoints } from '../../util/Values';
+import { getRequest, postRequest } from '../auth/auth.helper';
+
+export const friendService = {
+
+  async fetchFriendList({ token }) {
+    try {
+      const url = `${Endpoints.CORE}friend/list`;
+      const result = await getRequest({ token, url });
+      if (!result.ok) {
+        throw result;
+      }
+
+      return result.json();
+    } catch (err) {
+      console.log('ERROR: fetching friends: ', JSON.stringify(err));
+    }
+  },
+
+  async fetchFriendReqList({ token }) {
+    try {
+      const url = `${Endpoints.CORE}friend/request/list`;
+      const result = await getRequest({ token, url });
+      if (!result.ok) {
+        throw result;
+      }
+
+      return result.json();
+    } catch (err) {
+      console.log('Error fetching friend requests: ', JSON.stringify(err));
+    }
+  },
+
+  async initiateFriendRequest({ token, targetContactDetails, sharingLevel, sharingMessage }) {
+    try {
+      const url = `${Endpoints.CORE}friend/request/initiate`;
+      const params = { targetContactDetails, sharingLevel, sharingMessage };
+      const result = await postRequest({ token, url, params });
+    } catch (err) {
+      console.log('Error posting friend request: ', JSON.stringify(err));
+    }
+  },
+
+  async respondToFriendRequest({ token, response, requestId, sharingLevel }) {
+    try {
+      const url = `${Endpoints.CORE}friend/request/respond`;
+      const params = { requestId, response, sharingLevel };
+      const result = await postRequest({ token, url, params });
+    } catch (err) {
+      console.log('Error accepting friend request: ', JSON.stringify(err));
+    }
+  },
+
+  // will need to think through this carefully
+  async requestFriendToJoinBoost({ token, boostId, friendshipId }) {
+    try {
+      const url = `${Endpoints.CORE}friend/boost/add`;
+      const params = { boostId, friendshipId };
+      const result = await postRequest({ token, boostId, friendshipId });
+    } catch (err) {
+      console.log('Error inviting friend to boost: ', JSON.stringify(err));
+    }
+  },
+
+}
